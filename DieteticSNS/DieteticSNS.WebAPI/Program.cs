a4788da3273ec4177fa3c9d23ea7e209015a09a0
Pridecry <pridecry@gmail.com>
2019-08-31 10:31:48 +0200

    Added Clean Architecture structure

--- DieteticSNS/DieteticSNS.WebAPI/Program.cs
+++ DieteticSNS/DieteticSNS.WebAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace DieteticSNS
+namespace DieteticSNS.WebAPI
 {
     public class Program
     {
--- DieteticSNS/DieteticSNS.WebAPI/Startup.cs
+++ DieteticSNS/DieteticSNS.WebAPI/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace DieteticSNS
+namespace DieteticSNS.WebAPI
 {
     public class Startup
     {
