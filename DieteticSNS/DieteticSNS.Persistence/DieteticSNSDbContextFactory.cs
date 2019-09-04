715d84c5090ee2ce50742956fcdd4413746892ca
Pridecry <pridecry@gmail.com>
2019-09-05 00:50:15 +0200

    Added DbContextFactory

---
+++ DieteticSNS/DieteticSNS.Persistence/DieteticSNSDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Persistence.Infrastructure;
+
+namespace DieteticSNS.Persistence
+{
+    class DieteticSNSDbContextFactory : DesignTimeDbContextFactoryBase<DieteticSNSDbContext>
+    {
+        protected override DieteticSNSDbContext CreateNewInstance(DbContextOptions<DieteticSNSDbContext> options)
+        {
+            return new DieteticSNSDbContext(options);
+        }
+    }
+}
