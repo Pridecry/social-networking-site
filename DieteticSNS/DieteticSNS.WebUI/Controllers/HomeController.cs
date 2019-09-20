7e351ee064d9958f7a6615502a64084cb386ec64
k.dolegiewicz <k.dolegiewicz@infinity-group.pl>
2019-09-20 12:06:05 +0200

    temp commit

--- DieteticSNS/DieteticSNS.WebUI/Controllers/HomeController.cs
+++ DieteticSNS/DieteticSNS.WebUI/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ namespace DieteticSNS.WebUI.Controllers
             return View();
         }
 
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- DieteticSNS/DieteticSNS.WebUI/Controllers/IngredientsController.cs
+++ DieteticSNS/DieteticSNS.WebUI/Controllers/IngredientsController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using DieteticSNS.Application.Models.Ingredients.Commands.CreateIngredient;
+using DieteticSNS.Application.Models.Ingredients.Queries.GetIngredientDetails;
 using DieteticSNS.Application.Models.Ingredients.Queries.GetIngredientsList;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +9,34 @@ namespace DieteticSNS.WebUI.Controllers
     public class IngredientsController : BaseController
     {
         [HttpGet]
-        public async Task<ActionResult<IngredientListModel>> IngredientList()
+        public async Task<ActionResult<IngredientListModel>> GetIngredientList()
         {
             return View(await Mediator.Send(new GetIngredientListQuery()));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IngredientDetailsModel>> Get(int id)
+        {
+            return View(await Mediator.Send(new GetIngredientDetailsQuery { Id = id }));
+        }
+
+        [HttpGet]
+        public IActionResult CreateIngredient()
+        {
+            return View(new CreateIngredientCommand());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateIngredient(CreateIngredientCommand command)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(command);
+            }
+
+            await Mediator.Send(command);
+
+            return RedirectToAction(nameof(GetIngredientList));
         }
     }
 }
