90f9468ae95bac95e85f94f89235b493c499db43
k.dolegiewicz <k.dolegiewicz@infinity-group.pl>
2019-09-24 11:00:40 +0200

    Refactored layout files

---
+++ DieteticSNS/DieteticSNS.WebUI/wwwroot/js/showAdminPanel.js
@@ -0,0 +1,14 @@
+﻿function start() {
+    switchAdminPanel();
+}
+window.onload = start;
+
+function switchAdminPanel() {
+    const row = document.getElementById("mainRow");
+    const heading = document.getElementById("mainHeading");
+
+    row.classList.remove("bg-primary");
+    row.classList.add("bg-warning");
+
+    heading.innerHTML = "Dieteticer - Admin panel";
+}
